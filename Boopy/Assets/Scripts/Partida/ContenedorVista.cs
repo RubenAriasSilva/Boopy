6270373d8e608f4a98d0087c88fdd1c8aeeb7f23
RubenAriasSilva <arias.s.ruben26@gmail.com>
2025-12-03 15:29:16 -0600

    toggles en contenedor

--- Boopy/Assets/Scripts/Partida/ContenedorVista.cs
+++ Boopy/Assets/Scripts/Partida/ContenedorVista.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 
 namespace BoopyGame
@@ -10,6 +11,8 @@ namespace BoopyGame
         public PartidaControlador partidaControlador;
         public Jugador jugador1;
         public Jugador jugador2;
+
+        [Header("Cantidades de fichas")]
         public TextMeshProUGUI gatitosJugador1;
         public TextMeshProUGUI gatosJugador1;
         public TextMeshProUGUI gatitosJugador1Contrincante;
@@ -18,10 +21,18 @@ namespace BoopyGame
         public TextMeshProUGUI gatosJugador2;
         public TextMeshProUGUI gatitosJugador2Contrincante;
         public TextMeshProUGUI gatosJugador2Contrincante;
+
+        [Header("Nombres")]
         public TextMeshProUGUI textoJugador1Actual;
         public TextMeshProUGUI textoJugador2Actual;
         public TextMeshProUGUI textoJugador2;
         public TextMeshProUGUI textoJugador1;
+
+        [Header("Toggles de Selección")]
+        public Toggle toggleGatitoChicoP1;
+        public Toggle toggleGatoteP1;
+        public Toggle toggleGatitoChicoP2;
+        public Toggle toggleGatoteP2;
 
         public void Inicializar(PartidaControlador controlador, Jugador j1, Jugador j2)
         {
@@ -29,25 +40,33 @@ namespace BoopyGame
             jugador1 = j1;
             jugador2 = j2;
         }
-                
-        public void OnClickGatitoChicoP1()
-        {            
-            if(partidaControlador != null) partidaControlador.ClickEnContenedor(1, 1); // (jugador 1, tipo 1)
-        }
-        
-        public void OnClickGatoteP1()
+
+        public void Update()
         {
-            if(partidaControlador != null) partidaControlador.ClickEnContenedor(1, 2); // (jugador 1, tipo 2)
-        }
-        
-        public void OnClickGatitoChicoP2()
-        {
-            if(partidaControlador != null) partidaControlador.ClickEnContenedor(2, 1); // (jugador 2, tipo 1)
+            if (partidaControlador != null && toggleGatitoChicoP1.isOn) // (jugador 1, tipo 1)
+            {
+                partidaControlador.ClickEnContenedor(1, 1);
+            } 
+            if (partidaControlador != null && toggleGatoteP1.isOn)  // (jugador 1, tipo 2)
+            {
+                partidaControlador.ClickEnContenedor(1, 2);
+            } 
+            if (partidaControlador != null && toggleGatitoChicoP2.isOn)  // (jugador 2, tipo 1)
+            {
+                partidaControlador.ClickEnContenedor(2, 1);
+            } 
+            if (partidaControlador != null && toggleGatoteP2.isOn) 
+            {
+                partidaControlador.ClickEnContenedor(2, 2);
+            }
         }
 
-        public void OnClickGatoteP2 ()
+        public void ResetToggles()
         {
-            if(partidaControlador != null) partidaControlador.ClickEnContenedor(2, 2); // (jugador 2, tipo 2)
+            toggleGatitoChicoP1.isOn = false;
+            toggleGatoteP1.isOn = false;
+            toggleGatitoChicoP2.isOn = false;
+            toggleGatoteP2.isOn = false;
         }
 
         public void actualizarcontenedores()
